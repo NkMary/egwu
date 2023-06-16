0ab6cb64f8c612c2f7ba75e2ee8ab2ddca1e36de
Nkechinyere Urum-Eke <nk@nkechinyeresmbp.myfiosgateway.com>
2023-06-15 21:15:45 -0400

    added Nigerian states and changes colors

--- index.js
+++ index.js
@@ -1,26 +1,70 @@
+const inital = document.querySelector('.start');
+const guess = document.querySelector('input');
+const btn = document.querySelector('.btn');
+let score = document.querySelector('.score-1');
+score.innerHTML = 0;
+let sc = 0;
+let game = false;
+let newV = "";
+let randomV = "";
 
-const term = document.querySelector('.term');
-const definition = document.querySelector('.definition');
-const CheckButton = document.querySelector('.check');
-const nextButton = document.querySelector('.next');
+const words = ['Abia', 'Adamawa', 'Akwa Ibom', 'Anambra', 'Bauchi', 'Bayelsa', 'Benue', 'Borno', 'Benue', 'Borno','Cross River','Delta','Ebonyi','Edo','Ekiti', 'Enugu','Gombe','Imo','Jigawa', 'Kaduna','Kano','Katsina','Kebbi','Kogi','Kwara','Lago','Nasarawa', 'Niger', 'Ogun', 'Ondo', 'Osun', 'Oyo', 'Plateau', 'Rivers', 'Sokoto', 'Taraba','Yobe', 'Zamfara'];
 
-words = {
-  Cat: "Ewu",
-  Dog: "Nkita",
-  Bird: "Nkuku",
+const createWords = () => {
+    let number = Math.floor(Math.random() * words.length);
+    let random = words[number];
+    // console.log(random.split(""));
+    return random;
 }
-data = Object.entries(words)
 
-function getRandomWord() {
-  randomTerm = data[Math.floor(Math.random() * data.length)]
-  term.innerHTML = `<h3>${randomTerm[0]}</h3>`;
-  definition.innerHTML = `<h3>${randomTerm[1]}</h3>`;
+const jumbleWord = (el) => {
+    for (let i = el.length - 1; i >= 0; i--) {
+        let temp = el[i];
+        let j = Math.floor(Math.random() * (i + 1));
+        el[i] = el[j];
+        el[j] = temp;
+
+    }
+    return el.join("");
+
+
 }
 
-CheckButton.addEventListener('click', function() {
-  definition.style.display = 'block';
-});
+btn.addEventListener('click', function () {
+    if (!game) {
+        game = true;
+        inital.style.color = '#263A29';
 
-nextButton.addEventListener('click', function() {
-  getRandomWord();
-});
+        btn.textContent = 'Guess Word';
+        guess.classList.toggle('hidden');
+        newV = createWords();
+        randomV = jumbleWord(newV.split(""));
+        console.log(randomV);
+        inital.innerHTML = 'Arrange the word \'' + randomV + '\'';
+
+    } else {
+
+        let inputWord = guess.value;
+        if (inputWord === newV) {
+            console.log('correct');
+            game = false;
+            inital.innerHTML = `Correct!.\n
+                          It is ${newV}`;
+            inital.style.color = 'black';
+            sc += 5;
+            score.innerHTML = sc;
+            guess.classList.toggle('hidden');
+            btn.innerHTML = 'start again';
+            guess.value = "";
+
+        } else {
+            console.log('incorrect');
+            inital.innerHTML = `Better luck next time.Try Again! '${randomV}'`;
+            inital.style.color = '#263A29';
+            btn.innerHTML = 'Again';
+            guess.value = "";
+            guess.classList.toggle('hidden');
+        }
+
+    }
+})
