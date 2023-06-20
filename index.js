8316001f9c7789c1d74579eb9af9e8126c797b3e
Nkechinyere Urum-Eke <nk@nkechinyeresmbp.myfiosgateway.com>
2023-06-20 11:54:10 -0400

    created objects that contain placeholder information for each state

--- index.js
+++ index.js
@@ -8,10 +8,46 @@ let game = false;
 let newV = "";
 let randomV = "";
 
-const words = ['Abia', 'Adamawa', 'Akwa Ibom', 'Anambra', 'Bauchi', 'Bayelsa', 'Benue', 'Borno', 'Benue', 'Borno','Cross River','Delta','Ebonyi','Edo','Ekiti', 'Enugu','Gombe','Imo','Jigawa', 'Kaduna','Kano','Katsina','Kebbi','Kogi','Kwara','Lago','Nasarawa', 'Niger', 'Ogun', 'Ondo', 'Osun', 'Oyo', 'Plateau', 'Rivers', 'Sokoto', 'Taraba','Yobe', 'Zamfara'];
+const words = ['Abia', 'Adamawa', 'Akwa Ibom', 'Anambra', 'Bauchi', 'Bayelsa', 'Benue', 'Borno', 'Benue', 'Borno','Cross River','Delta','Ebonyi','Edo','Ekiti', 'Enugu','Gombe','Imo','Jigawa', 'Kaduna','Kano','Katsina','Kebbi','Kogi','Kwara','Lagos','Nasarawa', 'Niger', 'Ogun', 'Ondo', 'Osun', 'Oyo', 'Plateau', 'Rivers', 'Sokoto', 'Taraba','Yobe', 'Zamfara'];
 
 const stateInfo = {
+  'Abia': 'Abia is a state in the south eastern part of Nigeria.',
+  'Adamawa': 'Adamawa is a state in the south eastern part of Nigeria.',
+  'Akwa Ibom': 'Akwa Ibom is a state in the south eastern part of Nigeria.',
+  'Anambra': 'Anambra is a state in the south eastern part of Nigeria.',
+  'Bauchi': 'Bauchi is a state in the south eastern part of Nigeria.',
+  'Bayelsa': 'Bayelsa is a state in the south eastern part of Nigeria.',
+  'Benue': 'Benue is a state in the south eastern part of Nigeria.',
+  'Borno': 'Borno is a state in the south eastern part of Nigeria.',
+  'Cross River': 'Cross River is a state in the south eastern part of Nigeria.',
+  'Delta': 'Delta is a state in the south eastern part of Nigeria.',
+  'Ebonyi': 'Ebonyi is a state in the south eastern part of Nigeria.',
+  'Edo': 'Edo is a state in the south eastern part of Nigeria.',
+  'Ekiti': 'Ekiti is a state in the south eastern part of Nigeria.',
+  'Enugu': 'Enugu is a state in the south eastern part of Nigeria.',
+  'Gombe': 'Gombe is a state in the south eastern part of Nigeria.',
+  'imo': 'Imo is a state in the south eastern part of Nigeria.',
+  'Jigawa': 'Jigawa is a state in the south eastern part of Nigeria.',
+  'Kaduna': 'Kaduna is a state in the south eastern part of Nigeria.',
+  'Kano': 'Kano is a state in the south eastern part of Nigeria.',
+  'Kasina': 'Kasina is a state in the south eastern part of Nigeria.',
+  'Kebbi': 'Kebbi is a state in the south eastern part of Nigeria.',
+  'Kogi': 'Kogi is a state in the south eastern part of Nigeria.',
+  'Kwara': 'Kwara is a state in the south eastern part of Nigeria.',
+  'Lagos': 'Lagos is a state in the south eastern part of Nigeria.',
+  'Nasarawa': 'Nasarawa is a state in the south eastern part of Nigeria.',
+  'Niger': 'Niger is a state in the south eastern part of Nigeria.',
+  'Ogun': 'Ogun is a state in the south eastern part of Nigeria.',
   'Ondo': 'Ondo is a state in the south eastern part of Nigeria.',
+  'Osun': 'Osun is a state in the south eastern part of Nigeria.',
+  'Oyo': 'Oyo is a state in the south eastern part of Nigeria.',
+  'Plateau': 'Plateau is a state in the south eastern part of Nigeria.',
+  'Rivers': 'Rivers is a state in the south eastern part of Nigeria.',
+  'Sokoto': 'Sokoto is a state in the south eastern part of Nigeria.',
+  'Taraba': 'Taraba is a state in the south eastern part of Nigeria.',
+  'Kaduna': 'Kaduna is a state in the south eastern part of Nigeria.',
+  'Yobe': 'Yobe is a state in the south eastern part of Nigeria.',
+  'Zamfara': 'Zamfara is a state in the south eastern part of Nigeria.',
 };
 
 const createWords = () => {
